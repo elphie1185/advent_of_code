a6a42d5a314c44e9eb8fde478985680dff7c2e70
delphine_rabiller <delphine.rabiller@freeagent.com>
2023-12-05 16:52:18 +0000

    start on day 3

---
+++ day03/day_3.py
@@ -0,0 +1,20 @@
+import re
+
+not_symbols = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."]
+
+with open("day03/input.txt") as f:
+    lines = [line for line in f]
+
+print(lines[0])
+
+print(list(re.finditer("\d+", lines[0])))
+# get indices of the row with number
+
+#  check indice before and below for characters different from dot or num
+
+# get array of characters in rows above and below at indices -1 and +1
+#  check if any of the characters are different from a dot or a number
+# row_above = []
+# row_below = []
+
+# all(character in not_symbols for character in row_below)
